46f93fbf3349e107261007930a52b2a394d07493
Elvis Gomez <elvis@scriptiny.com>
2016-02-09 22:26:21 +0000

    Updated server.js, Posts are now saved into a persistent database. updated the GET ID to find the posts in the database. created a model folder and a data folder which includes the database

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -39,32 +41,27 @@ app.get('/todos', function (req, res) {
 // Todos GET ID
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedId = _.findWhere(todos, {id: todoId});
-
-	if (matchedId) {
-		res.json(matchedId);
-	} else {
-		res.status(404).send();
-	}
+	
+	db.todo.findById(todoId).then(function (todo) {
+		if (!!todo) {
+			res.json(todo.toJSON());
+		} else {
+			res.status(404).send('Not found!');
+		}
+	}, function (e) {
+		res.status(500).send();
+	});
 });
 
 //Todos Post /todos
 app.post('/todos', function (req, res) {
 	var body = _.pick(req.body, 'description', 'completed'); //picks only description and completed from req.body.
-
-	//if description and completed are not available and the description is null return bad request
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
-
-	//trim spaces in the description
-	body.description = body.description.trim();
-
-	body.id = todoNextId++; // inserts a new field into the object 'id'.
-
-	todos.push(body);
-
-	res.json(body);
+	
+	db.todo.create (body).then(function (todo) {
+		res.json(todo.toJSON());
+	}, function (e) {
+		res.status(400).json(e);
+	});
 });
 
 // Delete /todos/:id
@@ -109,6 +106,9 @@ app.put('/todos/:id', function (req, res) {
 	res.json(validAttributes);
 });
 
-app.listen(PORT, function () {
-    console.log('Express listening on port ' + PORT + '!');
+db.sequelize.sync().then(function () {
+	app.listen(PORT, function () {
+	    console.log('Express listening on port ' + PORT + '!');
+	});
 });
+
